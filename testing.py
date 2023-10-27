d9a20ed084e671c6c66dfa085edd33dde8ffbbd6
ravharvie <85156042+ravharvie@users.noreply.github.com>
2023-10-27 05:52:09 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -19,4 +19,4 @@ async def get_data (username: str):
 
     return{"results": data}
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
